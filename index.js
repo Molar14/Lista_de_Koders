ea0ee4a3828673efe6501053a9bc41a06bb0f2c1
Molar <grubenmj@gmail.com>
2024-05-23 20:35:24 -0600

    almost ready

---
+++ index.js
@@ -0,0 +1,112 @@
+const fs = require('node:fs')
+const express =require("express")
+const server = express()
+const lista = "koders.json"
+const fileExist = fs.existsSync(lista)
+server.use(express.json())
+if(!fileExist){
+    fs.writeFileSync(lista, JSON.stringify({koders:[]}))
+}
+function updateAlumnos(koders){
+    const newkoders = {koders: koders}
+    const newkodersAsString = JSON.stringify(newkoders)
+    fs.writeFileSync(lista,newkodersAsString)
+}
+function getAlumnos(){
+    const content =fs.readFileSync(lista,"utf8")
+    return JSON.parse(content).koders
+}
+function eliminar(nombre){
+    const arrayAlumnos = getAlumnos()
+    for(let i=0;i<arrayAlumnos.length;i++){
+        const comparativo=arrayAlumnos[i]
+        if(nombre===comparativo){
+            arrayAlumnos.splice(i, 1)
+        }
+    }
+    updateAlumnos(arrayAlumnos)
+}
+server.get('/koders', (request, response)=> {
+    const arrayAlumnos = getAlumnos()
+    if (!arrayAlumnos.length){
+        response.status(400)
+        response.json({
+            message: "Vacio"
+        })
+    }else{
+        response.status(400)
+        response.json({
+            message: "all koders",
+            koders: arrayAlumnos
+        })
+    }
+})
+server.post('/koders', (request, response)=> {
+    const arrayAlumnos = getAlumnos()   
+    const newKoder =  request.body
+    const newKoderName = request.body.name
+    const newKoderGen = request.body.Generacion
+    const newKoderGender = request.body.Genero
+    const newKoderAge = request.body.Edad
+    const newKoderActive = request.body.Activo
+    if(!newKoderName){
+        response.status(400)
+        response.json({
+            message: "Name required"
+        })
+        return
+    }
+    if(!newKoderGen){
+        response.status(400)
+        response.json({
+            message: "Generacion required"
+        })
+        return
+    }
+    if(!newKoderGender){
+        response.status(400)
+        response.json({
+            message: "Gender required"
+        })
+        return
+    }
+    if(!newKoderAge){
+        response.status(400)
+        response.json({
+            message: "Age required"
+        })
+        return
+    }
+    if(!newKoderActive){
+        response.status(400)
+        response.json({
+            message: "If is active required"
+        })
+        return
+    }
+    response.status(400)
+    arrayAlumnos.push(newKoder)
+    updateAlumnos(arrayAlumnos)
+    response.json({
+        message: "New koder added",
+        koders: arrayAlumnos
+    })
+})
+server.delete('/koders/:name', (request, response)=> {
+    const arrayAlumnos = getAlumnos()  
+    const KoderName =request.params.name
+    eliminar(KoderName)
+    response.json({
+        message: "Student deleted successfully",
+        koders: arrayAlumnos
+    })
+})
+server.delete('/koders', (request, response)=> {
+    updateAlumnos([])
+    response.json({
+        message: "SE FUE TODOOOO"
+    })
+})
+server.listen(8080,()=>{
+    console.log("server running on port 8080")
+})
